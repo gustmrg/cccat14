d5ef52a043f42233722cb522f492a164496ae682
Gustavo Miranda <gmiranda90@outlook.com>
2023-11-12 16:25:43 -0300

    Add function getAccount

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -1,73 +1,124 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
 
-export function validateCpf (cpf: string) {
-	if (!cpf) return false;
-	cpf = clean(cpf);
-	if (isInvalidLength(cpf)) return false;
-	if (allDigitsAreTheSame(cpf)) return false;
-	const dg1 = calculateDigit(cpf, 10);
-	const dg2 = calculateDigit(cpf, 11);
-	return extractCheckDigit(cpf) === `${dg1}${dg2}`;
+export function validateCpf(str: string) {
+	if (str !== null) {
+		if (str !== undefined) {
+			if (str.length >= 11 && str.length <= 14) {
+				// cleaning cpf
+				str = str
+					.replace('.', '')
+					.replace('.', '')
+					.replace('-', '')
+					.replace(" ", "");
+
+				if (!str.split("").every(c => c === str[0])) {
+					try {
+						let d1, d2;
+						let dg1, dg2, rest;
+						let digito;
+						let nDigResult;
+						d1 = d2 = 0;
+						dg1 = dg2 = rest = 0;
+
+						for (let nCount = 1; nCount < str.length - 1; nCount++) {
+							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
+							// 	return false;
+							// } else {
+
+							digito = parseInt(str.substring(nCount - 1, nCount));
+							d1 = d1 + (11 - nCount) * digito;
+
+							d2 = d2 + (12 - nCount) * digito;
+							// }
+						};
+
+						rest = (d1 % 11);
+
+						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+						d2 += 2 * dg1;
+						rest = (d2 % 11);
+						if (rest < 2)
+							dg2 = 0;
+						else
+							dg2 = 11 - rest;
+
+						let nDigVerific = str.substring(str.length - 2, str.length);
+						nDigResult = "" + dg1 + "" + dg2;
+						return nDigVerific == nDigResult;
+
+						// just in case...
+					} catch (e) {
+						console.error("Erro !" + e);
+
+						return false;
+					}
+				} else return false
+
+			} else return false;
+		}
+
+
+	} else return false;
 }
 
-function clean (cpf: string) {
-	return cpf.replace(/\D/g, "");
-}
-
-function isInvalidLength (cpf: string) {
-	return cpf.length !== 11;
-}
-
-function allDigitsAreTheSame (cpf: string) {
-	return cpf.split("").every(c => c === cpf[0]);
-}
-
-function calculateDigit (cpf: string, factor: number) {
-	let total = 0;
-	for (const digit of cpf) {
-		if (factor > 1) total += parseInt(digit) * factor--;
-	}
-	const rest = total%11;
-	return (rest < 2) ? 0 : 11 - rest;
-}
-
-function extractCheckDigit (cpf: string) {
-	return cpf.slice(9);
-}
-
-export async function signup (input: any): Promise<any> {
+export async function signup(input: any): Promise<any> {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
-		const accountId = crypto.randomUUID();
-		const [account] = await connection.query("select * from cccat14.account where email = $1", [input.email]);
-		if (account) throw new Error("Duplicated account");
-		if (isInvalidName(input.name)) throw new Error("Invalid name");
-		if (isInvalidEmail(input.email)) throw new Error("Invalid email");
-		if (!validateCpf(input.cpf)) throw new Error("Invalid cpf");
-		if (input.isDriver && isInvalidCarPlate(input.carPlate)) throw new Error("Invalid car plate");
-		await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-		return {
-			accountId
-		};
+		const id = crypto.randomUUID();
+
+		const [acc] = await connection.query("select * from cccat14.account where email = $1", [input.email]);
+		if (!acc) {
+
+			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
+				if (input.email.match(/^(.+)@(.+)$/)) {
+
+					if (validateCpf(input.cpf)) {
+						if (input.isDriver) {
+							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+								await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
+
+								const obj = {
+									accountId: id
+								};
+								return obj;
+							} else {
+								// invalid car plate
+								return -5;
+							}
+						} else {
+							await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
+
+							const obj = {
+								accountId: id
+							};
+							return obj;
+						}
+					} else {
+						// invalid cpf
+						return -1;
+					}
+				} else {
+					// invalid email
+					return -2;
+				}
+
+			} else {
+				// invalid name
+				return -3;
+			}
+
+		} else {
+			// already exists
+			return -4;
+		}
+
 	} finally {
 		await connection.$pool.end();
 	}
 }
 
-function isInvalidName (name: string) {
-	return !name.match(/[a-zA-Z] [a-zA-Z]+/);
-}
-
-function isInvalidEmail (email: string) {
-	return !email.match(/^(.+)@(.+)$/);
-}
-
-function isInvalidCarPlate (carPlate: string) {
-	return !carPlate.match(/[A-Z]{3}[0-9]{4}/);
-}
-
-export async function getAccount (accountId: string) {
+export async function getAccount(accountId: string) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	const [account] = await connection.query("select * from cccat14.account where account_id = $1", [accountId]);
 	await connection.$pool.end();
--- backend/ride/src/main_refactored.ts
+++ backend/ride/src/main_refactored.ts
@@ -1,68 +1,68 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
 
-export function validateCpf (str: string) {
+export function validateCpf(str: string) {
 	if (str !== null) {
 		if (str !== undefined) {
-			if (str.length >= 11 && str.length <= 14){
+			if (str.length >= 11 && str.length <= 14) {
 				// cleaning cpf
-				str=str
-					.replace('.','')
-					.replace('.','')
-					.replace('-','')
-					.replace(" ","");  
-	
+				str = str
+					.replace('.', '')
+					.replace('.', '')
+					.replace('-', '')
+					.replace(" ", "");
+
 				if (!str.split("").every(c => c === str[0])) {
-					try{  
-						let     d1, d2;  
-						let     dg1, dg2, rest;  
-						let     digito;  
-							let     nDigResult;  
-						d1 = d2 = 0;  
-						dg1 = dg2 = rest = 0;  
-							
-						for (let nCount = 1; nCount < str.length -1; nCount++) {  
+					try {
+						let d1, d2;
+						let dg1, dg2, rest;
+						let digito;
+						let nDigResult;
+						d1 = d2 = 0;
+						dg1 = dg2 = rest = 0;
+
+						for (let nCount = 1; nCount < str.length - 1; nCount++) {
 							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
 							// 	return false;
 							// } else {
-	
-								digito = parseInt(str.substring(nCount -1, nCount));  							
-								d1 = d1 + ( 11 - nCount ) * digito;  
-				
-								d2 = d2 + ( 12 - nCount ) * digito;  
+
+							digito = parseInt(str.substring(nCount - 1, nCount));
+							d1 = d1 + (11 - nCount) * digito;
+
+							d2 = d2 + (12 - nCount) * digito;
 							// }
-						};  
-							
-						rest = (d1 % 11);  
-				
-						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-						d2 += 2 * dg1;  
-						rest = (d2 % 11);  
-						if (rest < 2)  
-							dg2 = 0;  
-						else  
-							dg2 = 11 - rest;  
-				
-							let nDigVerific = str.substring(str.length-2, str.length);  
-						nDigResult = "" + dg1 + "" + dg2;  
+						};
+
+						rest = (d1 % 11);
+
+						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+						d2 += 2 * dg1;
+						rest = (d2 % 11);
+						if (rest < 2)
+							dg2 = 0;
+						else
+							dg2 = 11 - rest;
+
+						let nDigVerific = str.substring(str.length - 2, str.length);
+						nDigResult = "" + dg1 + "" + dg2;
 						return nDigVerific == nDigResult;
-						
-					// just in case...
-					}catch (e){  
-						console.error("Erro !"+e);  
-	
-						return false;  
-					}  
+
+						// just in case...
+					} catch (e) {
+						console.error("Erro !" + e);
+
+						return false;
+					}
 				} else return false
-	
-			}else return false;
+
+			} else return false;
 		}
 
 
 	} else return false;
 }
 
-export async function signup (input: any): Promise<any> {
+export async function signup(input: any): Promise<any> {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
 		const id = crypto.randomUUID();
@@ -77,7 +77,7 @@ export async function signup (input: any): Promise<any> {
 						if (input.isDriver) {
 							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
 								await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-								
+
 								const obj = {
 									accountId: id
 								};
