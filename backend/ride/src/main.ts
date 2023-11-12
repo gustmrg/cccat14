aca2afa90bc278e573e6323ced7e1663c211851b
Gustavo Miranda <gmiranda90@outlook.com>
2023-11-12 18:53:22 -0300

    Refactor signup method

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -11,7 +11,6 @@ export function validateCpf(str: string) {
 					.replace('.', '')
 					.replace('-', '')
 					.replace(" ", "");
-
 				if (!str.split("").every(c => c === str[0])) {
 					try {
 						let d1, d2;
@@ -50,68 +49,40 @@ export function validateCpf(str: string) {
 						// just in case...
 					} catch (e) {
 						console.error("Erro !" + e);
-
 						return false;
 					}
 				} else return false
-
 			} else return false;
 		}
-
-
 	} else return false;
 }
 
+function isInvalidName(name: string) {
+	return !name.match(/[a-zA-Z] [a-zA-Z]+/)
+}
+
+function isInvalidEmail(email: string) {
+	return !email.match(/^(.+)@(.+)$/);
+}
+
+function isInvalidCarPlate(carPlate: string) {
+	return !carPlate.match(/[A-Z]{3}[0-9]{4}/);
+}
+
 export async function signup(input: any): Promise<any> {
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+	const connection = pgp()("postgresql://postgres:postgrespw@localhost:5432/cccat14?schema=public");
 	try {
-		const id = crypto.randomUUID();
+		const accountId = crypto.randomUUID();
+		const [account] = await connection.query("select * from account where email = $1", [input.email]);
+		if (account) throw new Error("Duplicated account");
+		if (isInvalidName(input.name)) throw new Error("Invalid name");
+		if (isInvalidEmail(input.email)) throw new Error("Invalid email");
+		if (!validateCpf(input.cpf)) throw new Error("Invalid cpf");
+		if (input.isDriver && isInvalidCarPlate(input.carPlate)) throw new Error("Invalid car plate");
 
-		const [acc] = await connection.query("select * from cccat14.account where email = $1", [input.email]);
-		if (!acc) {
+		await connection.query("insert into account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
 
-			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (input.email.match(/^(.+)@(.+)$/)) {
-
-					if (validateCpf(input.cpf)) {
-						if (input.isDriver) {
-							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-								const obj = {
-									accountId: id
-								};
-								return obj;
-							} else {
-								// invalid car plate
-								return -5;
-							}
-						} else {
-							await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							return obj;
-						}
-					} else {
-						// invalid cpf
-						return -1;
-					}
-				} else {
-					// invalid email
-					return -2;
-				}
-
-			} else {
-				// invalid name
-				return -3;
-			}
-
-		} else {
-			// already exists
-			return -4;
-		}
+		return { accountId };
 
 	} finally {
 		await connection.$pool.end();
@@ -119,8 +90,8 @@ export async function signup(input: any): Promise<any> {
 }
 
 export async function getAccount(accountId: string) {
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	const [account] = await connection.query("select * from cccat14.account where account_id = $1", [accountId]);
+	const connection = pgp()("postgresql://postgres:postgrespw@localhost:5432/cccat14?schema=public");
+	const [account] = await connection.query("select * from account where account_id = $1", [accountId]);
 	await connection.$pool.end();
 	return account;
 }
